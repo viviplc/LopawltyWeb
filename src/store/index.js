1f1f17b8a4705bc0c174e7fdcb334c909ffb5bbf
Romeno Wenogk Fernando <drf325@nyu.edu>
2022-04-10 12:57:50 -0400

    configured vuex-persist dependancy

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersistence from "vuex-persist";
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage,
+});
 
 Vue.use(Vuex)
 
@@ -11,5 +16,6 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
-  }
+  },
+  plugins: [vuexLocal.plugin],
 })
